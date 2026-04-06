239767c5e202147eee7c242fe1240803ac96cf8a
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2026-04-06 17:45:49 +0500

    Create common_types.h

---
+++ src/common_types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+#include <stdexcept>
+#include <cassert>
+#include <functional>
+#include <queue>
+
+#include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vk_mem_alloc.h>
+#include <VkBootstrap.h>
+#include <GLFW/glfw3.h>
